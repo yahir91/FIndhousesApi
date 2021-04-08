cfabe3ccd6a0b5ca42331c3b17636cb25810129c
Yahir Cardona <yahir.cardona@gmail.com>
2021-04-08 15:46:50 -0600

    sessionStore initialez modified to work locally

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,9 +28,8 @@ class SessionsController < ApplicationController
         end
     end
 
-    def
+    def logged_out
         reset_session
         render json: {status:200, logged_out: true}
     end
-
 end

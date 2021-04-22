04972f4322f154baafadacf3441fe3fc42b466fa
Yahir Cardona <yahir.cardona@gmail.com>
2021-04-22 03:37:33 -0600

    session store domain updated again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_authentication_app', expire_after: 100.days, domain: :all
+Rails.application.config.session_store :cookie_store, key: '_authentication_app'

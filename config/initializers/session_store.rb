67431e950e4dd6bdbdbcfbc1ba256e6613486db3
Yahir Cardona <yahir.cardona@gmail.com>
2021-04-22 03:06:37 -0600

    session store domain updated again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,1 @@
-if Rails.env == 'production'
-Rails.application.config.session_store :cookie_store, key: '_authentication_app', expire_after: 100.days, domain: 'https://houses-api-yahir.herokuapp.com'
-else
-Rails.application.config.session_store :cookie_store, key: '_authentication_app'
-end
+Rails.application.config.session_store :cookie_store, key: '_authentication_app', expire_after: 100.days, domain: :all

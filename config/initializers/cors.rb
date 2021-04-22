f46915ed0ed04f534370695c31e41ac4cc5a2f1b
Yahir Cardona <yahir.cardona@gmail.com>
2021-04-22 03:33:16 -0600

    session store domain updated again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,6 @@
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
-     allow do
-         origins 'http://localhost:3000'
-         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-     end
-     allow do
-        origins 'http://localhost:3000'
+    allow do
+        origins 'https://yahir-houses-frontend.herokuapp.com' 
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
  end
